d19a548d3f589da3d9181623a4ed5029478cd0cc
dodysenz@gmail.com <dodysenz@gmail.com>
2020-08-04 02:43:53 +0800

    pref(ngrx): add store devtools predicate

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,11 +5,27 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
+
+const routerStoreFilterPredicate = function (
+	actionType: string,
+	res: boolean = true,
+): { actionType: string; res: boolean } {
+	return {
+		actionType,
+		res: res && actionType.startsWith('@ngrx'),
+	};
+};
+
 @NgModule({
 	imports: [
 		CommonModule,
 		StoreModule.forRoot({}, {}),
-		StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+		StoreDevtoolsModule.instrument({
+			maxAge: 25,
+			logOnly: environment.production,
+			name: 'Seizure Detector',
+			predicate: actionType => routerStoreFilterPredicate(actionType).res,
+		}),
 		EffectsModule.forRoot([]),
 		StoreRouterConnectingModule.forRoot(),
 	],
