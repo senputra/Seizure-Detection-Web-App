5a24b86f333ae876fc498095380b1ca0ff25c961
Dodee <dodysenz@gmail.com>
2020-09-02 01:36:58 +0800

    refactor: change logger to prepare for production

--- src/app/core/devServices/logger.service.ts
+++ src/app/core/devServices/logger.service.ts
@@ -1,21 +1,41 @@
 import { Injectable, Inject } from '@angular/core';
+import { environment } from '@environment/environment';
 
 @Injectable({ providedIn: 'root' })
 export class Logger {
-  constructor(@Inject(String) private header: string) {}
+  private production = false;
+  constructor(@Inject(String) private header: string) {
+    this.production = environment.production;
+    if (this.production) {
+      console.log(
+        'Hello there! The app is running in production mode. There will be no console log except for errors.',
+      );
+    } else {
+      console.log('Console log is online! Develop your way!');
+    }
+  }
 
   // tslint:disable-next-line: no-any
   get log(): (...args: any[]) => void {
+    if (this.production) {
+      return () => {};
+    }
     return console.log.bind(console, this.header);
   }
 
   // tslint:disable-next-line: no-any
   get warn(): (...args: any[]) => void {
+    if (this.production) {
+      return () => {};
+    }
     return console.warn.bind(console, this.header);
   }
 
   // tslint:disable-next-line: no-any
   get error(): (...args: any[]) => void {
+    if (this.production) {
+      return () => {};
+    }
     return console.error.bind(console, this.header);
   }
 }
