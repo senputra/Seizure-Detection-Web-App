47fc16c3c08f527535f22d944a28d278847c94ea
Dodee <dodysenz@gmail.com>
2020-09-18 21:28:05 +0800

    V1.0.0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,16 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+const firebaseConfig = {
+  apiKey: 'AIzaSyAfvb1uticoLGO3AeNsil7ktFio8ARQnZ8',
+  authDomain: 'medicalapp-a3e35.firebaseapp.com',
+  databaseURL: 'https://medicalapp-a3e35.firebaseio.com',
+  projectId: 'medicalapp-a3e35',
+  storageBucket: 'medicalapp-a3e35.appspot.com',
+  messagingSenderId: '416082366353',
+  appId: '1:416082366353:web:b9bad358f40a2711d39430',
+  measurementId: 'G-Z1FGDFFSRM',
+};
 
 export const environment = {
   production: false,
@@ -8,16 +18,7 @@ export const environment = {
     GENERAL_KEY: 'abc',
     SPECIALIST_KEY: '123',
   },
-  firebaseConfig: {
-    apiKey: 'AIzaSyC-0k9eLQZi7WGmFGgSUyhj7YOY230zmss',
-    authDomain: 'seizure-detection-3b9dc.firebaseapp.com',
-    databaseURL: 'https://seizure-detection-3b9dc.firebaseio.com',
-    projectId: 'seizure-detection-3b9dc',
-    storageBucket: 'seizure-detection-3b9dc.appspot.com',
-    messagingSenderId: '75491667771',
-    appId: '1:75491667771:web:ea80657c36cc26f08f911e',
-    measurementId: 'G-J73908GNEY',
-  },
+  firebaseConfig,
 };
 
 /*
